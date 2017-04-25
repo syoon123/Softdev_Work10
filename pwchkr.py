342ed3ccc42c11280346adb47759a26702aed952
Jordan Yaqoob <jyaqoob@stuy.edu>
2017-04-25 00:40:16 -0400

    finished strength test

--- pwchkr.py
+++ pwchkr.py
@@ -10,11 +10,38 @@ def check(pwd):
         print("Good password")
         return True
 
+
+
 #Tests
 check("Pass123")
 check("password")
 check("Password")
 check("123")
 
-def check2(pwd):
+
+def strength(pwd):
+    upper = len([ c for c in pwd if ord(c) in range(65, 91, 1) ])
+    lower = len([ c for c in pwd if ord(c) in range(97, 123, 1) ])
+    numbers = len([ c for c in pwd if ord(c) in range(48, 58, 1) ])
+    special = len(pwd) - upper - lower - numbers
+    plen = len(pwd)
+    if not check(pwd):
+        return 0
+    else:
+        if upper > 2:
+            upper = 2.5
+        if lower > 2:
+            lower = 2.5
+        if numbers > 2:
+            numbers = 2.5
+        if special > 2:
+            special = 2.5
+        return upper + lower + numbers + special
+    
+            
+print strength("jordan123")
+print strength("Jordan123")
+print strength("JoRdAn&*^*&!AS!(1238")
+    
+
     
